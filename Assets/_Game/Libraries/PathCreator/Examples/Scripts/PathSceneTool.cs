f5d2f4a9dc4b5303882defb80566cbdc36d147b0
mikewazzz <ipektaskin9@gmail.com>
2021-07-10 22:09:16 +0300

    scene update added

---
+++ Assets/_Game/Libraries/PathCreator/Examples/Scripts/PathSceneTool.cs
@@ -0,0 +1,32 @@
+﻿using PathCreation;
+using UnityEngine;
+
+namespace _Game.Libraries.PathCreator.Examples.Scripts
+{
+    [ExecuteInEditMode]
+    public abstract class PathSceneTool : MonoBehaviour
+    {
+        public event System.Action onDestroyed;
+        public PathCreation.PathCreator pathCreator;
+        public bool autoUpdate = true;
+
+        protected VertexPath path {
+            get {
+                return pathCreator.path;
+            }
+        }
+
+        public void TriggerUpdate() {
+            PathUpdated();
+        }
+
+
+        protected virtual void OnDestroy() {
+            if (onDestroyed != null) {
+                onDestroyed();
+            }
+        }
+
+        protected abstract void PathUpdated();
+    }
+}
